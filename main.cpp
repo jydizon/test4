a2a55228057a7ac7f8afd77a5fedccd1f9135c50
jydizon <47217807+jydizon@users.noreply.github.com>
2019-04-27 22:42:57 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include<cstdio>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include "menu.h"
+using namespace std;
+
+int main() {
+  staff s;
+  s.menu();
+  return 0;
+}
